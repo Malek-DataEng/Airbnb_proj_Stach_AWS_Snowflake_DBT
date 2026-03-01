e033dd163914a16825404442afcdccf46854dd39
Malek-DataEng <abbar.malek@outlook.fr>
2026-03-02 00:11:04 +0100

    update val where booking_amount in source_test.sql

--- Airbnb_Proj_DBT_Snowflake/airbnb_proj_dbt_snowflake/tests/source_test.sql
+++ Airbnb_Proj_DBT_Snowflake/airbnb_proj_dbt_snowflake/tests/source_test.sql
@@ -2,14 +2,8 @@
 -- It will return all the bookings with a booking amount less than 0
 -- If it returns any rows, it means the source is not working
 
-{{ config(
-    severity = 'warn',
-    )
-}}
-
-
 select 
     booking_amount 
 from 
     {{ source('staging', 'bookings') }}
-where booking_amount < 200
+where booking_amount < 0
